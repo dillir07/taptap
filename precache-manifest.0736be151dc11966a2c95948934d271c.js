f4d8996ab9820a0f7f915aea54d00e3f6dfbff8e
Dilli Babu R <dillir07@outlook.com>
2020-06-22 20:27:45 +0530

    Updates

---
+++ precache-manifest.0736be151dc11966a2c95948934d271c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2640cd2a818660a7d82e8c7ffba080",
+    "url": "/taptap/index.html"
+  },
+  {
+    "revision": "60d8dd270c8b8667a979",
+    "url": "/taptap/static/css/main.1d5dfa68.chunk.css"
+  },
+  {
+    "revision": "6fb1b69b56bffcfb646b",
+    "url": "/taptap/static/js/2.3ed59d93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taptap/static/js/2.3ed59d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d8dd270c8b8667a979",
+    "url": "/taptap/static/js/main.73be164f.chunk.js"
+  },
+  {
+    "revision": "918ef1a97161bef05bb3",
+    "url": "/taptap/static/js/runtime-main.7caecbe7.js"
+  }
+]);
